d06dc38f401e9a646f8cc3eee4903ea607942af6
mafizul247 <mafizul247@gmail.com>
2026-05-13 10:39:38 +0600

    Google Map

--- src/pages/GoogleMap/MapGoogleApp.jsx
+++ src/pages/GoogleMap/MapGoogleApp.jsx
@@ -2,11 +2,12 @@ import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 
 const MapGoogleApp = () => {
+    // https://visgl.github.io/react-google-maps/docs/get-started
     // npm install @vis.gl/react-google-maps
 
     return (
         <APIProvider apiKey="AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao" >
-            <h1>Google Map</h1>
+            <h1 className="text-xl font-bold text-center my-4">Google Map</h1>
             <Map
                 style={{ width: '100vw', height: '100vh' }}
                 defaultCenter={{ lat: 22.54992, lng: 0 }}
